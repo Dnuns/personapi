049bab231fcc6eeafcc39aed8526b36364c79057
David Nunes <51808846+Dnuns@users.noreply.github.com>
2022-02-12 21:51:23 -0100

    update personController

--- src/main/java/one/digitalinovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinovation/personapi/controller/PersonController.java
@@ -13,30 +13,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import one.digitalinovation.personapi.dto.request.PersonDTO;
 import one.digitalinovation.personapi.dto.response.MessageResponseDTO;
-import one.digitalinovation.personapi.service.PersonService;
+import one.digitalinovation.personapi.repository.PersonRepository;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
+
+	private PersonRepository personRepository;
 	
 	@Autowired
-	private PersonService personService;
+	public PersonController(PersonRepository personRepository) {
+		this.personRepository = personRepository;
+	}
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
 		
-		return personService.createPerson(personDTO);
+		return personRepository.createPerson(personDTO);
 	}
 	
 	@GetMapping
 	public List<PersonDTO> listAll(){
-		return personService.listAll();
+		return personR.listAll();
 	}
 }
--- src/main/java/one/digitalinovation/personapi/repository/PersonRepository.java
+++ src/main/java/one/digitalinovation/personapi/repository/PersonRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import one.digitalinovation.personapi.entity.Person;
 
-public interface PersonRepository extends JpaRepository<Person, Long> {
-	
+public interface PersonRepository extends JpaRepository<Person, Long> {	
 }
